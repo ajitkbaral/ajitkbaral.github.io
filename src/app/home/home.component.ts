0bd764d3af5e27a59c7cd2ebc557b26a4674db67
Ajit Kumar Baral <ajitkbaralofficial@gmail.com>
2019-12-17 18:26:41 +0545

    Added personal works and contact

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-home",
@@ -6,6 +6,30 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  public menuItems = [
+    "Home",
+    "About",
+    "Portfolio",
+    "Skills",
+    "Personal works",
+    "Contact"
+  ];
+
+  @ViewChild("home")
+  public home: ElementRef;
+  @ViewChild("about")
+  public about: ElementRef;
+  @ViewChild("portfolio")
+  public portfolio: ElementRef;
+  @ViewChild("mySkills")
+  public mySkills: ElementRef;
+  @ViewChild("personalWork")
+  public personalWork: ElementRef;
+  @ViewChild("contact")
+  public contact: ElementRef;
+
+  public currentMenu;
+
   public skills = [
     {
       name: "Java",
@@ -36,9 +60,59 @@ export class HomeComponent implements OnInit {
       percent: 75
     }
   ];
-  public options: any;
+
+  public personalWorks = [
+    {
+      title: "Service Prov",
+      type: "Android Application",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur iure asperiores voluptatum nisi doloribus, beatae, repellat ipsam fugiat nostrum cum nemo ducimus. Nihil dolorem vero reiciendis laboriosam impedit velit?",
+      logoName: "service_prov_logo.png",
+      imageName: "service_prov_app.png"
+    },
+    {
+      title: "Air Pay",
+      type: "Android Application",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur iure asperiores voluptatum nisi doloribus, beatae, repellat ipsam fugiat nostrum cum nemo ducimus. Nihil dolorem vero reiciendis laboriosam impedit velit?",
+      logoName: "airpay_logo.png",
+      imageName: "airpay_app.png"
+    }
+  ];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setMenu(this.menuItems[0]);
+  }
+
+  public setMenu(menu) {
+    this.currentMenu = menu;
+    let elementRef: ElementRef;
+    switch (menu) {
+      case "About":
+        elementRef = this.about;
+        break;
+      case "Portfolio":
+        elementRef = this.portfolio;
+        break;
+      case "Skills":
+        elementRef = this.mySkills;
+        break;
+      case "Personal works":
+        elementRef = this.personalWork;
+        break;
+      case "Contact":
+        elementRef = this.contact;
+        break;
+      default:
+        elementRef = this.home;
+    }
+    this.scrollToMenu(elementRef);
+  }
+
+  private scrollToMenu(elementRef: ElementRef) {
+    elementRef.nativeElement.scrollIntoView({ behavior: "smooth" });
+    this.currentMenu = this.menuItems[0];
+  }
 }
