b89d03847c8e0980c34a838eece9dd51025cd435
Ajit Kumar Baral <kkecnepal@gmail.com>
2019-12-21 11:19:28 +0545

    Firebase added in prod file with skill UI change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-ZbAr1uc07zjSt9YJvTBPhK6idCIPJDc",
+    authDomain: "my-website-7008f.firebaseapp.com",
+    databaseURL: "https://my-website-7008f.firebaseio.com",
+    projectId: "my-website-7008f",
+    storageBucket: "my-website-7008f.appspot.com",
+    messagingSenderId: "977853593021",
+    appId: "1:977853593021:web:d93360269b3f8fb43fced4",
+    measurementId: "G-VGW54S0V1W"
+  }
 };
